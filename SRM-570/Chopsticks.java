9dc0cb7779d2bcb3653fd5e5fa34900e3516a7b7
brhCS <benjamin.hipple@gmail.com>
2013-04-03 00:57:07 -0300

    Div II, 250 points

--- SRM-570/Chopsticks.java
+++ SRM-570/Chopsticks.java
@@ -1,1 +1,28 @@
-v
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import java.math.*;
+import java.awt.geom.*;
+
+public class Chopsticks
+{
+  public int getmax(int[] length)
+	{
+		int count = 0;
+		Integer n;
+		HashSet<Integer> hash = new HashSet<Integer>();
+		for(int i=0; i<length.length; i++) {
+			n = length[i];		
+			boolean match = hash.add(n);
+			
+			if(!match) {
+				count++;
+				hash.remove(n);
+			}
+		}
+		
+		return count;
+	}
+	
+}
+//Powered by [KawigiEdit] 2.0!
