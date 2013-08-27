3e5e7e9c57eb14cf3e67f36cb03f0ca2041094a4
brhCS <benjamin.hipple@gmail.com>
2013-08-26 20:27:10 -0400

    Div II, 500 points

---
+++ SRM-587/JumpFurther.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import java.math.*;
+
+
+public class JumpFurther
+{
+public int furthest(int N, int badStep)
+{
+boolean hitBad = false;
+int onStep = 0;
+for(int i=1; i<=N; i++) {
+onStep = onStep + i;
+if(onStep == badStep) {
+hitBad = true;
+break;
+}	
+}
+int sum = N * (N + 1) / 2;
+
+return hitBad ? (sum - 1) : sum;
+}
+}
