13f11bd9ca91f8ed3d6b948e4fc8a858abdb48b9
brhCS <benjamin.hipple@gmail.com>
2013-09-24 20:59:15 -0400

    Div II, 250 points

---
+++ SRM-591/TheArithmeticProgression.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import java.math.*;
+
+
+public class TheArithmeticProgression
+{
+	public double minimumChange(int a, int b, int c)
+	{
+		return Math.abs(b - ((c + a) / 2D));
+	}
+}
+//Powered by KawigiEdit 2.1.4 (beta) modified by pivanof!
