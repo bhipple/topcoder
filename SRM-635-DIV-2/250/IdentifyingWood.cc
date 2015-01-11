a9805e43cfd65b2e70d052074e7a282243c60050
Benjamin Hipple <bhipple@bloomberg.net>
2015-01-11 03:15:13 -0500

    250 and 500

---
+++ SRM-635-DIV-2/250/IdentifyingWood.cc
@@ -0,0 +1,52 @@
+
+// {{{ $VIMCODER$ <-----------------------------------------------------
+// vim:filetype=cpp:foldmethod=marker:foldmarker={{{,}}}
+
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+// }}}
+
+#define ZERO(a) memset(a, 0, sizeof(a))
+#define FOR(x,to) for(x=0;x<to;++x)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();++x)
+
+class IdentifyingWood
+{
+public:
+    string check(string s, string t)
+    {
+        int N = t.length();
+        int matIdx = 0;
+        for(int i = 0; i < s.length(); ++i) {
+            if(matIdx < N && s[i] == t[matIdx]) {
+                ++matIdx;
+            }
+        }
+        if(matIdx == N) return "Yep, it's wood.";
+        return "Nope.";
+    }
+};
+
---
+++ SRM-635-DIV-2/500/QuadraticLaw.cc
@@ -0,0 +1,51 @@
+
+// {{{ $VIMCODER$ <-----------------------------------------------------
+// vim:filetype=cpp:foldmethod=marker:foldmarker={{{,}}}
+
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+// }}}
+
+#define ZERO(a) memset(a, 0, sizeof(a))
+#define FOR(x,to) for(x=0;x<to;++x)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();++x)
+
+class QuadraticLaw
+{
+public:
+    long long getTime(long long d)
+    {
+        long long late = 0;
+
+        while(late + (late * late) <= d) {
+            ++late;
+        }
+        if(late + late * late > d) --late;
+
+        return late;
+    }
+};
+
